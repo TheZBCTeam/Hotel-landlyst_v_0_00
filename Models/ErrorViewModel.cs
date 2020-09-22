5ef96f101eb242194d0ff1ddc376aeb818278321
Webzonedk <webzone@kpdesign.dk>
2020-09-22 11:39:46 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hotel_landlyst_v_0_00.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
